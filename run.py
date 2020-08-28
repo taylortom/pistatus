c9d608b0c8437ba35d61400d40a37341e5967145
Tom Taylor <hello@tomtaylor.name>
2020-08-28 13:30:33 +0100

    Add timer function to button

--- run.py
+++ run.py
@@ -3,14 +3,23 @@ import github;
 import timer;
 import write;
 
-def on_press(button_name):
-    if button_name == "A":
-        write.scroll(str(github.getContributions()))
-    elif button_name == "B":
-        write.scroll(github.getStatus())
-    elif button_name == "X":
-        write.scroll(timer.getTime())
-    elif button_name == "Y":
-        write.scroll("Y")
+class App:
+    def __init__(self):
+        self.timer = False
+        buttons.listen(self.on_press)
 
-buttons.listen(on_press)
+    def on_press(self, button_name):
+        if button_name == "A":
+            write.scroll(str(github.getContributions()))
+        elif button_name == "B":
+            write.scroll(github.getStatus())
+        elif button_name == "X":
+            write.scroll(timer.getTime())
+        elif button_name == "Y":
+            if self.timer == False:
+                self.timer = timer.createTimer()
+                write.scroll("Timer started")
+            else: 
+                write.scroll(self.timer.getRemainingStr(), "255,0,0", 1)
+
+App()
