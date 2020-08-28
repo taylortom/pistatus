33e2b17ab8f6974b968abe40457401f15cb46c79
Tom Taylor <hello@tomtaylor.name>
2020-08-28 15:09:53 +0100

    Make rainbow the default colour

--- write.py
+++ write.py
@@ -14,24 +14,38 @@ class Colours():
         self.Blue = "0,0,255"
         self.White = "255,255,255"
         self.Black = "0,0,0"
+	self.Rainbow = "Rainbow" # note: special case
 
     def strToColour(self, s):
+	if s == "Rainbow":
+            return self.Rainbow
+
+        if s == "Red":
+            s = self.Red
+        if s == "Green":
+            s = self.Green
+        if s == "Blue":
+            s = self.Blue
+        if s == "White":
+            s = self.White
+
         r, g, b = s.split(",")
         return [int(r), int(g), int(b)]
 
-default_colour = "255,255,255"
-default_show_total = 2
 COLOURS = Colours()
-
+DEFAULT_COLOUR = COLOURS.Rainbow
+DEFAULT_BRIGHTNESS = 0.1
+DEFAULT_ROTATION = 0
+DEFAULT_SHOW_TOTAL = 2
 
 def init_unicorn():
     unicornhatmini = UnicornHATMini()
     unicornhatmini.clear()
-    unicornhatmini.set_rotation(0)
-    unicornhatmini.set_brightness(0.1)
+    unicornhatmini.set_rotation(DEFAULT_ROTATION)
+    unicornhatmini.set_brightness(DEFAULT_BRIGHTNESS)
     return unicornhatmini;
 
-def scroll(text, colour = COLOURS.White, show_total = 2):
+def scroll(text, colour = DEFAULT_COLOUR, show_total = DEFAULT_SHOW_TOTAL):
     uni = init_unicorn()
 
     display_width, display_height = uni.get_shape()
@@ -43,12 +57,16 @@ def scroll(text, colour = COLOURS.White, show_total = 2):
 
     show_count = 0
     offset_x = 0
-    r, g, b = COLOURS.strToColour(colour)
+
+    if colour != COLOURS.Rainbow:
+        r, g, b = COLOURS.strToColour(colour)
 
     while show_count < show_total:
         for y in range(display_height):
             for x in range(display_width):
                 hue = (time.time() / 10.0) + (x / float(display_width * 2))
+                if colour == COLOURS.Rainbow:
+                    r, g, b = [int(c * 255) for c in hsv_to_rgb(hue, 1.0, 1.0)]
                 if image.getpixel((x + offset_x, y)) == 255:
                     uni.set_pixel(x, y, r, g, b)
                 else:
