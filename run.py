b85a19698aa6912450de002e6e1c99bad77d8943
Tom Taylor <hello@tomtaylor.name>
2020-08-28 12:30:24 +0100

    Add handlers for all button presses

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 import buttons;
 import github;
+import timer;
 import write;
 
 def on_press(button_name):
     if button_name == "A":
-        c = github.getContributions()
-        print(c)
-        write.scroll(str(c))
+        write.scroll(str(github.getContributions()))
+    elif button_name == "B":
+        write.scroll(github.getStatus())
+    elif button_name == "X":
+        write.scroll(timer.getTime())
+    elif button_name == "Y":
+        write.scroll("Y")
 
 buttons.listen(on_press)
