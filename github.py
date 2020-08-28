23d2c9caa0ec573f7ce1e2196d4b5ecb8de3e5c7
Tom Taylor <hello@tomtaylor.name>
2020-08-28 01:06:03 +0100

    Refactor to return data

--- github.py
+++ github.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 import requests
-import write
 
-def writeContributions(contributions):
+def getContributions():
     url = 'http://192.168.1.94:5000/api/github'
     r = requests.get(url).json()
-    newContributions = int(r["contributions"])
-    if newContributions != contributions:
-        write.scroll(str(newContributions))
-        return newContributions
+    return int(r["contributions"])
