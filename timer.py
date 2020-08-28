102c17bc8ff51d92a1c8a0ca54ca7c982bf07297
Tom Taylor <hello@tomtaylor.name>
2020-08-28 13:30:46 +0100

    Add timer implementation

--- timer.py
+++ timer.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 import datetime
+import time
 
 class Timer:
     def __init__(self):
-        self.time = 0
+        self.TIMER_LENGTH = 45 * 60
+        self.start = time.time()
+        self.end = self.start + self.TIMER_LENGTH
+
+    def getRemaining(self):
+        left = time.localtime(self.end - time.time());
+        return [left.tm_min, left.tm_sec]
+
+    def getRemainingStr(self):
+        [m, s] = self.getRemaining()
+        return str(m) + ":" + str(s)
+
+    def getRemainingProgress(self):
+        return round(((self.end - time.time())/self.TIMER_LENGTH)*100)
+        
 
 def getTime():
     return datetime.datetime.now().strftime("%H:%M")
 
 def createTimer():
     return Timer()
-
-
