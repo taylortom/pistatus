db922125058b2bce5e873974f5dbc6be5d69c405
Tom Taylor <hello@tomtaylor.name>
2020-09-02 11:21:20 +0100

    Make path absolute

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import json
+import os
 
 class Config:
     def __init__(self):
-        self.config = json.load(open('config.json',))
+        self.config = json.load(open(os.path.dirname(os.path.realpath(__file__)) + '/config.json'))
 
     def get(self, key):
         return self.config[key]
